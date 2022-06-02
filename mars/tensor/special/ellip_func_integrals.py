1bb2880edc0c85007c4ed7d1686ebd13ba3362c5
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2022-06-01 21:07:34 -0700

    Update ellip_func_integrals.py

--- mars/tensor/special/ellip_func_integrals.py
+++ mars/tensor/special/ellip_func_integrals.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from xml.dom.minidom import Attr
 import scipy.special as spspecial
 
 from ..arithmetic.utils import arithmetic_operand
